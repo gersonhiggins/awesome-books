df459f2495cde49a8949511c54ddebf7874dd166
gersonhiggins <gahg.10gerson@gmail.com>
2023-04-25 17:28:43 -0400

    Add Menu Function

--- scripts/main.js
+++ scripts/main.js
@@ -88,3 +88,16 @@ if (localStorage.getItem('books')) {
   library.booksArr = JSON.parse(localStorage.getItem('books'));
   library.displayBooks();
 }
+
+const navLink = Array.from(document.querySelectorAll('.nav-link'));
+navLink.forEach(n => n.addEventListener('click', () => {
+  const navContent = Array.from(document.querySelectorAll('.content'));
+  navContent.forEach(e => {
+    if(navLink.indexOf(n) === navContent.indexOf(e)){
+      e.classList.remove('hidden');
+    }
+    else if(navLink.indexOf(n) !== navContent.indexOf(e)){
+      e.classList.add('hidden');
+    }
+  })
+}));
