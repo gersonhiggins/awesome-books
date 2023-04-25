164d93eb203c585d39a3c7c13fdbb7a71f76f9d3
benja27 <50673336+benja27@users.noreply.github.com>
2023-04-25 09:19:10 -0600

    fix linter errors

--- scripts/main.js
+++ scripts/main.js
@@ -34,25 +34,22 @@ class Books {
     this.booksArr.forEach((element, index) => {
       const node1 = document.createElement('div');
       node1.setAttribute('class', 'book');
-      node1.className = "d-flex align-items-center py-1 book"
+      node1.className = 'd-flex align-items-center py-1 book';
       this.container.appendChild(node1);
       const node2 = document.createElement('p');
       node2.setAttribute('class', 'text');
-      node2.classList = "m-0"
+      node2.classList = 'm-0';
       node2.innerText = `${element.name} by`;
       node1.appendChild(node2);
-      const node3 = document.createElement('p');      
+      const node3 = document.createElement('p');
       node3.innerText = `${element.author}`;
-      node3.className = "ps-2 m-0"
+      node3.className = 'ps-2 m-0';
       node1.appendChild(node3);
       const node4 = document.createElement('button');
       node4.innerText = 'remove';
       node4.setAttribute('class', 'remove');
       node4.setAttribute('data-id', `${index}`);
       node1.appendChild(node4);
-      // const span = document.createElement('span');
-      // span.setAttribute('class', 'book-span');
-      // node1.appendChild(span);
     });
   }
 
